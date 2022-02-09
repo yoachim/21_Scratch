cebf7c4916abbbd3b78bdb4742269d36ffcf3843
Peter Yoachim <yoachim@uw.edu>
2022-02-09 11:32:54 -0800

    more minion updates

--- minion_update/update_min.py
+++ minion_update/update_min.py
@@ -8,12 +8,12 @@ from astropy.time import Time
 
 
 
-def update_minion(outfile='minion_1016_update.db'):
+def update_minion(outfile='minion_1016_update.db', apply_weather=True, use_dithered=False):
     """Let's update the minion_1016 runs to have the current sky, weather downtime, and throughputs.
     """
 
     conn = sqlite3.connect('minion_1016_sqlite.db')
-    query = 'select fieldRA, fieldDec, filter, expMJD, night, visitExpTime, visitTime, airmass, rotSkyPos, rotTelPos, altitude, azimuth, slewTime from Summary group by expMJD order by expMJD'
+    query = 'select * from Summary group by expMJD order by expMJD'
     df = pd.read_sql(query, conn)
 
     num_obs = df['fieldRA'].size
@@ -22,8 +22,12 @@ def update_minion(outfile='minion_1016_update.db'):
     blank = empty_observation()
 
     observations = np.zeros(num_obs, dtype=blank.dtype)
-    observations['RA'] = df['fieldRA']
-    observations['dec'] = df['fieldDec']
+    if use_dithered:
+        observations['RA'] = df['ditheredRA']
+        observations['dec'] = df['ditheredDec']
+    else:
+        observations['RA'] = df['fieldRA']
+        observations['dec'] = df['fieldDec']
     observations['mjd'] = df['expMJD']
     observations['filter'] = df['filter']
     observations['exptime'] = df['visitExpTime']
@@ -35,26 +39,39 @@ def update_minion(outfile='minion_1016_update.db'):
     observations['rotTelPos'] = df['rotTelPos']
     observations['visittime'] = df['visitTime']
 
-    obs_good = np.zeros(num_obs, dtype=bool)
+    obs_good = np.ones(num_obs, dtype=bool)
 
-    for i, obs in enumerate(observations):
+    if apply_weather:
+        for i, obs in enumerate(observations):
 
-        obs_good[i] = True #mo.check_up(obs['mjd'])
-        clouds = mo.cloud_data(Time(obs['mjd'], format='mjd'))
-        if clouds > mo.cloud_limit:
-            obs_good[i] = False
+            obs_good[i] = True 
+            
+            clouds = mo.cloud_data(Time(obs['mjd'], format='mjd'))
+            if clouds > mo.cloud_limit:
+                obs_good[i] = False
 
-        if obs_good[i]:
-            mo.mjd = obs['mjd']
+            if obs_good[i]:
+                mo.mjd = obs['mjd']
 
-            observations[i] = mo.observation_add_data(obs)
+                observations[i] = mo.observation_add_data(obs)
 
-            progress = i/num_obs*100
-            text = "\rprogress=%.3f%%, %i of %i" % (progress, i, num_obs)
-            sys.stdout.write(text)
-            sys.stdout.flush()
+                progress = i/num_obs*100
+                text = "\rprogress=%.3f%%, %i of %i" % (progress, i, num_obs)
+                sys.stdout.write(text)
+                sys.stdout.flush()
+    else:
+        observations['airmass'] = df['airmass']
+        observations['FWHMeff'] = df['FWHMeff']
+        observations['FWHM_geometric'] = df['FWHMgeom']
+        observations['skybrightness'] = df['filtSkyBrightness']
+        observations['night'] = df['night']
+        observations['slewdist'] = df['slewDist']
+        observations['fivesigmadepth'] = df['fiveSigmaDepth']
+        observations['pa'] = df['phaseAngle']
+        observations['clouds'] = df['transparency']
+        observations['sunAlt'] = df['sunAlt']
+
         
-    
     observations = observations[np.where(obs_good == True)[0]]
     if outfile is not None:
         info = run_info_table(mo)
@@ -63,4 +80,5 @@ def update_minion(outfile='minion_1016_update.db'):
 
 
 if __name__ == '__main__':
-    update_minion()
+    #update_minion()
+    update_minion(outfile='minion_1016_newschema.db', apply_weather=False, use_dithered=True)
